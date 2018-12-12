8a6b94900619eb878170a7ce7ef25b0cc801c4cf
indefined <37869994+indefined@users.noreply.github.com>
2018-12-13 01:27:50 +0800

    推特youtube视频全屏脚本：初提交

---
+++ YTVideosInTwitterToFullscreen/YTVideosInTwitterToFullscreen.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         allow YouTube videos in twitter to fullscreen
+// @name:zh-CN   允许推特YouTube视频全屏
+// @namespace    indefined
+// @version      0.1
+// @description  make embedded YouTube video in twitter able to fullscreen
+// @description:zh-CN  使推特中嵌入的YouTube视频可以全屏
+// @author       indefined
+// @include      http*://twitter.com/i/cards*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    [].forEach.call(document.body.querySelectorAll('iframe[data-src*="youtube"]'),frame=>{
+        //console.log(frame);
+        frame.setAttribute('allowfullscreen', 'true');
+    });
+})();
