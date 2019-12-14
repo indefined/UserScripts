baad9f16505988a5fd7773419b4c8921308bf650
indefined <37869994+indefined@users.noreply.github.com>
2019-12-14 15:39:57 +0800

    [BiliOEP] 一个没什么卵用的替换播放器

---
+++ bilibiliOldEmbedPlayer/bilibiliOldEmbedPlayer.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Bilibili替换旧版播放器
+// @namespace    indefined
+// @version      0.1
+// @description  采用替换播放器的方式返回旧版（问题众多，不保证长久可用）
+// @author       indefined
+// @supportURL   https://github.com/indefined/UserScripts/issues
+// @include      /^https?:\/\/www\.bilibili\.com\/(video\/av|bangumi\/play\/(ss|ep))\d+/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const bfq = document.getElementById('bofqi');
+    if (bfq&&window.cid&&window.aid&&!document.querySelector('.vip')) {
+        const frame = document.createElement('iframe');
+        frame.src = `https://www.bilibili.com/blackboard/html5player.html?aid=${window.aid}&cid=${window.cid}&enable_ssl=1&crossDomain=1`;
+        bfq.replaceWith(frame);
+        const styleDiv = document.body.appendChild(document.createElement('style'));
+        function setSize() {
+            const wrap = document.querySelector('.v-wrap')||document.getElementById('app'),
+                  w = (wrap||frame.parentNode).clientWidth;
+            frame.style = `width: ${w}px; height:${wrap?w/1.5:frame.parentNode.clientHeight}px; border:none;`;
+            styleDiv.innerHTML = wrap?`.r-con #danmukuBox{height:${w/1.5}px !important}#player_module{height:unset!important}.plp-r{margin-top:${w/1.5+16}px}`:'';
+        }
+        window.addEventListener('resize',setSize);
+        setSize();
+    }
+    window.history.pushState = window.history.replaceState = function() {location.href = arguments[2];}
+})();
