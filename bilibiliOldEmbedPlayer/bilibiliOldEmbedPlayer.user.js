7ecbbb9ec40c1eb279c0f911c51bd131156e985e
indefined <37869994+indefined@users.noreply.github.com>
2019-12-15 17:14:29 +0800

    [BiliOEP]没什么用的更新

--- bilibiliOldEmbedPlayer/bilibiliOldEmbedPlayer.user.js
+++ bilibiliOldEmbedPlayer/bilibiliOldEmbedPlayer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili替换旧版播放器
 // @namespace    indefined
-// @version      0.1
+// @version      0.1.1
 // @description  采用替换播放器的方式返回旧版（问题众多，不保证长久可用）
 // @author       indefined
 // @supportURL   https://github.com/indefined/UserScripts/issues
@@ -21,7 +21,7 @@
             const wrap = document.querySelector('.v-wrap')||document.getElementById('app'),
                   w = (wrap||frame.parentNode).clientWidth;
             frame.style = `width: ${w}px; height:${wrap?w/1.5:frame.parentNode.clientHeight}px; border:none;`;
-            styleDiv.innerHTML = wrap?`.r-con #danmukuBox{height:${w/1.5}px !important}#player_module{height:unset!important}.plp-r{margin-top:${w/1.5+16}px}`:'';
+            styleDiv.innerHTML = wrap?`.r-con>#danmukuBox{height:${w/1.5}px !important}.r-con>.members-info+#danmukuBox{display:none}.members-info{margin-top:${w/1.5+100}px!important}#player_module{height:unset!important}.plp-r{margin-top:${w/1.5+16}px}`:'';
         }
         window.addEventListener('resize',setSize);
         setSize();
