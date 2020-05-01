f1f70ab6c70895af7e6656449f06ee1b9752216c
indefined <37869994+indefined@users.noreply.github.com>
2020-05-01 17:38:05 +0800

    [WCAE] 修复队列在视野中判断逻辑;修复停止队列逻辑;修复跳过条件判断...

--- weiboCommentAutoExpand/weiboCommentAutoExpand.user.js
+++ weiboCommentAutoExpand/weiboCommentAutoExpand.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微博自动展开评论
 // @namespace    indefined
-// @version      0.1.1
+// @version      0.1.2
 // @supportURL   https://github.com/indefined/UserScripts/issues
 // @description  自动展开微博详情页面中的评论，包括楼中楼，可配置队列延迟
 // @notice       可通过 localStorage.autodelay 自定义队列时间
@@ -16,26 +16,40 @@
     function qa(a,b) {return (b||document).querySelectorAll(a)}
     const list = [];
     let running;
-    function isInView(node) {
-        return list.indexOf(node) == -1 &&
+    function notInList(node) {
+        return list.indexOf(node) == -1;
+    }
+    function clickInView(node) {
+        if (
+            node &&
             node.offsetTop >= document.documentElement.scrollTop &&
-            node.offsetTop < document.documentElement.scrollTop + window.innerHeight + 200;
+            node.offsetTop < document.documentElement.scrollTop + window.innerHeight + 200
+           ) {
+            //console.log('click',list.length)
+            node.click();
+            return true;
+        }
     }
     function runList() {
+        running = true;
+        const clicked = clickInView(list.shift())
         if(list.length==0) {
+            //console.log('stop',list.length)
             running = false;
             return;
         }
-        running = true;
-        list.shift().click();
-        if(!q('.list_con .W_loading:not([style])')) {
+        if(!clicked || !q('.list_con .W_loading:not([style])')) {
+            //console.log('skip',list.length)
             runList();
         }
         else setTimeout(runList,localStorage.autodelay||1000);
     }
     function append(selector) {
-        list.push(...Array.from(qa(selector)).filter(isInView));
-        if(!running) runList();
+        list.push(...Array.from(qa(selector)).filter(notInList));
+        if(!running) {
+            //console.log('start',list.length)
+            runList();
+        }
     }
     function run() {
         if(q('.WB_tab')) {
