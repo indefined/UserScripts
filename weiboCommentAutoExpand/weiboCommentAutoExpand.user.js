cda02f6d8cf4b99bf9699ee67375e5963275e26e
indefined <37869994+indefined@users.noreply.github.com>
2020-05-01 16:58:41 +0800

    [WCAE] 自动展开加入可配置延迟...

--- weiboCommentAutoExpand/weiboCommentAutoExpand.user.js
+++ weiboCommentAutoExpand/weiboCommentAutoExpand.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         微博自动展开评论
 // @namespace    indefined
-// @version      0.1
+// @version      0.1.1
 // @supportURL   https://github.com/indefined/UserScripts/issues
-// @description  自动展开微博详情页面中的评论，包括楼中楼
+// @description  自动展开微博详情页面中的评论，包括楼中楼，可配置队列延迟
+// @notice       可通过 localStorage.autodelay 自定义队列时间
 // @author       indefined
 // @include      /https?:\/\/weibo\.com/\d+/.+/
 // @grant        none
@@ -13,16 +14,36 @@
     'use strict';
     function q(a,b) {return (b||document).querySelector(a)}
     function qa(a,b) {return (b||document).querySelectorAll(a)}
+    const list = [];
+    let running;
     function isInView(node) {
-        return node.offsetTop >= document.documentElement.scrollTop && node.offsetTop < document.documentElement.scrollTop + window.innerHeight + 200;
+        return list.indexOf(node) == -1 &&
+            node.offsetTop >= document.documentElement.scrollTop &&
+            node.offsetTop < document.documentElement.scrollTop + window.innerHeight + 200;
+    }
+    function runList() {
+        if(list.length==0) {
+            running = false;
+            return;
+        }
+        running = true;
+        list.shift().click();
+        if(!q('.list_con .W_loading:not([style])')) {
+            runList();
+        }
+        else setTimeout(runList,localStorage.autodelay||1000);
+    }
+    function append(selector) {
+        list.push(...Array.from(qa(selector)).filter(isInView));
+        if(!running) runList();
     }
     function run() {
         if(q('.WB_tab')) {
             window.removeEventListener('scroll',run);
             return;
         }
-        Array.from(qa('.WB_text a[action-data*="more_comment"]')).filter(isInView).forEach(item=>item.click());
-        Array.from(qa('.list_box .WB_cardmore .more_txt')).filter(isInView).forEach(item=>item.click());
+        append('.WB_text a[action-data*="more_comment"]');
+        append('.list_box .WB_cardmore .more_txt');
     }
     window.addEventListener('scroll',run);
 })();
