ec58b123b4ca8cdc943432e7c351f028749137db
indefined <37869994+indefined@users.noreply.github.com>
2020-06-05 21:58:11 +0800

    [WCAE]不在未登录条件下执行

--- weiboCommentAutoExpand/weiboCommentAutoExpand.user.js
+++ weiboCommentAutoExpand/weiboCommentAutoExpand.user.js
@@ -1,19 +1,23 @@
 // ==UserScript==
 // @name         微博自动展开评论
 // @namespace    indefined
-// @version      0.1.2
+// @version      0.1.3
 // @supportURL   https://github.com/indefined/UserScripts/issues
-// @description  自动展开微博详情页面中的评论，包括楼中楼，可配置队列延迟
+// @description  自动点击展开微博详情页面中的评论，包括楼中楼，可配置队列延迟
 // @notice       可通过 localStorage.autodelay 自定义队列时间
 // @author       indefined
 // @include      /https?:\/\/weibo\.com/\d+/.+/
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
     'use strict';
+    if (!window.$CONFIG||window.$CONFIG.islogin!=1) return;
+
     function q(a,b) {return (b||document).querySelector(a)}
     function qa(a,b) {return (b||document).querySelectorAll(a)}
+    function debug() {localStorage.debug&&console.log(...arguments)}
     const list = [];
     let running;
     function notInList(node) {
@@ -25,7 +29,7 @@
             node.offsetTop >= document.documentElement.scrollTop &&
             node.offsetTop < document.documentElement.scrollTop + window.innerHeight + 200
            ) {
-            //console.log('click',list.length)
+            debug('click',list.length)
             node.click();
             return true;
         }
@@ -34,12 +38,12 @@
         running = true;
         const clicked = clickInView(list.shift())
         if(list.length==0) {
-            //console.log('stop',list.length)
+            debug('stop',list.length)
             running = false;
             return;
         }
         if(!clicked || !q('.list_con .W_loading:not([style])')) {
-            //console.log('skip',list.length)
+            debug('skip delay',list.length)
             runList();
         }
         else setTimeout(runList,localStorage.autodelay||1000);
@@ -47,7 +51,7 @@
     function append(selector) {
         list.push(...Array.from(qa(selector)).filter(notInList));
         if(!running) {
-            //console.log('start',list.length)
+            debug('start',list.length)
             runList();
         }
     }
