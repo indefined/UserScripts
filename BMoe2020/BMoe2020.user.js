ed42dca550a62953c3101ca06a57ac310ff9f7e7
indefined <37869994+indefined@users.noreply.github.com>
2020-12-07 02:53:20 +0800

    [BMoe2020]乘除倒转;以最低票差估算最低票范围;单独处理每个投票区

--- BMoe2020/BMoe2020.user.js
+++ BMoe2020/BMoe2020.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BMoe2020
 // @namespace    indefined
-// @version      0.1.3.1
+// @version      0.1.4
 // @description  计(穷)算(举)2020年度动画大选实际票数
 // @author       indefined
 // @include      https://www.bilibili.com/blackboard/AOY2020.html*
@@ -34,30 +34,27 @@
     //new MutationObserver(console.log).observe(qa('.t-space-container.plat-section-space')[2], {childList:true, subtree:true})
     //qa('.t-space-container.plat-section-space')[2].insertAdjacentHTML('afterbegin', '<span id="start-culc" style="background: white;text-align: center;font-size: 50px;cursor: pointer;">计算</span>');
     //q('#start-culc').onclick = start;
+    document.head.insertAdjacentHTML('beforeend', '<style>.voted-progress>input[type="number"] {width: 90px;}</style>');
     qa('.t-space-container.plat-section-space')[2].insertAdjacentHTML('afterbegin', '<span id="clear-storage" style="background: white;text-align: center;font-size: 50px;cursor: pointer;margin-left: 140px;" title="如果出现票数倍数错误，点击重置并刷新页面重试">重置票数存储</span>');
     q('#clear-storage').onclick = ()=>delete localStorage.bmoe2020;
+
+    let datas = JSON.parse(localStorage.bmoe2020||'[[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],3]');
+    if (datas[6]!=3) {
+        //增加一位判断数据版本，防止数据出错需要重置
+        datas = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],3];
+    }
     const timmer = setInterval(()=>{
-        if (qa('.voted-container2').length==6){
-            clearInterval(timmer);
-            start();
-        }
-    },1000);
-    function start(){
-        //this.remove();
-        let datas = JSON.parse(localStorage.bmoe2020||'[[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],3]');
-        if (datas[6]!=3) {
-            //增加一位判断数据版本，防止数据出错需要重置
-            datas = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],3];
-        }
         qa('.content-container').forEach((container,index)=>{
-            //if (index>0) return;
+            if (container.classList.contains('handled')||!q('.voted-container2', container)) return;
+            container.classList.add('handled');
+            console.time(index);
             const votes = qa('.progress-container', container).map(vote=>{
                 const percent = parseFloat(q('.progress-line', vote).style.width);
                 if (!percent) return;
                 const input = document.createElement('input');
                 input.type = 'number';
                 input.percent = percent;
-                input.onchange = function(){recount(input.percent/input.value);show()};
+                input.onchange = function(){recount(input.value/input.percent);show()};
                 vote.insertAdjacentElement('afterend',input);
                 return {percent, input};
             }).filter(i=>i);
@@ -65,10 +62,10 @@
             const total = container.children[0].insertAdjacentElement('beforeend', document.createElement('span'));
             function show() {
                 votes.forEach(vote=>(vote.input.value = vote.value));
-                total.textContent = votes.reduce((a,b)=>(a+b.value),0) - votes[0].value;
+                total.textContent = (votes.reduce((a,b)=>(a+b.value),0) - votes[0].value).toLocaleString();
             }
-            function recount(rate) {
-                votes.forEach(vote=>(vote.value = vote.percent/rate));
+            function recount(total) {
+                votes.forEach(vote=>(vote.value = vote.percent*total));
             }
             function checkHead() {
                 for (let i = 0; i< 20; i++) {
@@ -88,25 +85,37 @@
             const tail = votes[votes.length-1],
                   head = votes[1];
             let finsh = false;
-            for (let t = datas[index][0]; t<150; t++) {
-                recount(tail.percent/t);
+            let min = 1;
+            //计算最小票差
+            for (let i = (votes.length/2).toFixed(0); i<votes.length-1; i++) {
+                let diff = votes[i-1].percent - votes[i].percent;
+                if (diff==0) continue;
+                min = Math.min(min, diff);
+            }
+            //以最小票差为1算最低票不超过percent/min，为防止误差*2
+            for (let t = datas[index][0]; t<tail.percent/min*2; t++) {
+                recount(t/tail.percent);
                 if (!checkTail()) continue;
+                console.log(index, 'tail', t);
                 const reduce = head.value/50;
                 head.value = Math.max(datas[index][1],Math.floor(head.value - reduce));
                 for (let k = 0; k<reduce*2; k++) {
-                    recount(head.percent/head.value)
+                    recount(head.value/head.percent)
                     if (checkHead()) {
                         finsh = true;
                         datas[index] = [tail.value.toFixed(0), head.value.toFixed(0)];
+                        console.log(index, t, k)
+                        save(datas);
+                        show();
                         break;
                     }
                     head.value += 1;
                 }
                 if (finsh) break;
             }
-            show();
+            console.timeEnd(index);
             container.children[0].onclick = ()=>copy(votes.map(v=>`${v.input.nextElementSibling.textContent}\t${v.input.previousSibling.textContent}\t${v.value.toFixed(1)}\t${v.input.previousSibling.querySelector('span.progress-line').style.width}`).join('\r\n'));
         });
-        save(datas);
-    }
+        if (qa('.handled').length==6) clearInterval(timmer);
+    },1000);
 })();
