1e335fdc42307d1f2225d114d779514a33bfc65d
indefined <37869994+indefined@users.noreply.github.com>
2021-04-16 13:55:34 +0800

    [BLIVE] 轮播换P

--- bilibiliLive/blivePlus.user.js
+++ bilibiliLive/blivePlus.user.js
@@ -2,7 +2,7 @@
 // @name        bilibili直播间工具
 // @namespace   indefined
 // @supportURL  https://github.com/indefined/UserScripts/issues
-// @version     0.5.33
+// @version     0.5.34
 // @author      indefined
 // @description 可配置 直播间切换勋章/头衔、硬币直接购买勋章、礼物包裹替换为大图标、网页全屏自动隐藏礼物栏/全屏发送弹幕(仅限HTML5)、轮播显示链接(仅限HTML5)
 // @include     /^https?:\/\/live\.bilibili\.com\/(blanc\/)?\d/
@@ -316,16 +316,15 @@ body.fullscreen-fix #live-player div~div#gift-control-vm,
                 style:'margin-left:16px;font-size:16px'
             });
             this.titleObserver = new MutationObserver(mutations => {
-                mutations.forEach((mutation)=>{
-                    if (/web-player-round-title|bilibili-live-player-video-round-title/.test(mutation.target.className)) {
-                        this.updateVideoLink();
-                    }
-                    for (const addedNode of mutation.addedNodes) {
-                        if (/web-player-round-title|bilibili-live-player-video-round-title/.test(addedNode.className)) {
+                //console.log(mutations);
+                for (const mutation of mutations) {
+                    for (const node of mutation.addedNodes) {
+                        if (node instanceof HTMLVideoElement) {
                             this.updateVideoLink();
+                            return;
                         }
                     }
-                });
+                }
             });
             this.fullScreenObserver = new MutationObserver(mutations => this.handleFullScreenPanel());
         },
@@ -363,12 +362,12 @@ body.fullscreen-fix #live-player div~div#gift-control-vm,
                 helper.xhr('https://api.live.bilibili.com/room/v1/Room/room_init?id='+roomid)
                   .then(roominfo => {
                           roominfo.data && helper.xhr('https://api.live.bilibili.com/live/getRoundPlayVideo?room_id='+roominfo.data.room_id+'?type=flv')
-                          .then(resp => 
-                          {
+                          .then(resp => {
                             console.log(resp.data);
-                            resp.data.bvid && helper.set(this.title, 
-                              {innerText: resp.data.bvid,
-                                href:`${resp.data.bvid_url}?p=${resp.data.pid}`}, titlePanel);
+                            resp.data.bvid && helper.set(this.title, {
+                                innerText: resp.data.bvid,
+                                href:`${resp.data.bvid_url}?p=${resp.data.pid}`
+                            }, titlePanel);
                           });
                   });
             }
