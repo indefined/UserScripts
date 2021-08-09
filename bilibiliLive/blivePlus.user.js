8ad53327d8c84ce6e30314d364f4fec930b7b057
indefined <37869994+indefined@users.noreply.github.com>
2021-08-09 22:26:15 +0800

    [BLIVE] fixed #72 修复活动直播间时间同步功能

--- bilibiliLive/blivePlus.user.js
+++ bilibiliLive/blivePlus.user.js
@@ -2,7 +2,7 @@
 // @name        bilibili直播间工具
 // @namespace   indefined
 // @supportURL  https://github.com/indefined/UserScripts/issues
-// @version     0.5.41
+// @version     0.5.43
 // @author      indefined
 // @description 可配置 直播间切换勋章/头衔、礼物包裹替换为大图标、网页全屏自动隐藏礼物栏/全屏发送弹幕(仅限HTML5)、轮播显示链接(仅限HTML5)
 // @include     /^https?:\/\/live\.bilibili\.com\/(blanc\/)?\d/
@@ -494,14 +494,14 @@ body.fullscreen-fix #live-player div~div#gift-control-vm,
     timeSync: {
         status: false,
         init(settings) {
-            if (!helper.window.livePlayer) return;
+            try{if (!helper.window.livePlayer && !helper.window.top.livePlayer) return;}catch(e){return console.error(e);}
             this.icon = helper.create('span',{
                 title: "同步时间",
                 className: 'icon',
                 innerHTML: `<svg t="1595680402158" style="width: 22px; height: 36px;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7532" width="22" height="22"><path d="M938.1888 534.016h-80.7936c0.4096-7.3728 0.6144-14.6432 0.6144-22.016 0-218.624-176.8448-400.7936-389.12-400.7936C257.024 111.2064 80.6912 293.1712 80.6912 512c0 218.7264 176.4352 400.7936 388.1984 400.7936 74.752 0 149.0944-22.016 208.1792-60.0064l42.7008 68.608c-75.0592 48.9472-161.9968 74.8544-250.7776 74.752C209.8176 996.1472 0 779.264 0 512S209.8176 27.8528 468.8896 27.8528C728.3712 27.8528 938.7008 244.736 938.7008 512c0 7.3728-0.2048 14.6432-0.512 22.016z m-261.12 318.7712z m-26.4192-158.1056L426.7008 556.032V291.9424h64v226.5088L689.5616 635.904l-38.912 58.7776z m245.3504-6.656L768 512h256L896 688.0256z" fill="#ffffff" p-id="7533"></path></svg>`,
                 onclick: this.setVideoSync
             });
-            helper.window.livePlayer.on('ctrlVisibleChange', status=>{
+            (helper.window.livePlayer||helper.window.top.livePlayer).on('ctrlVisibleChange', status=>{
                 if (status && this.status) this.append();
             });
             this.update('timeSync', settings.timeSync);
