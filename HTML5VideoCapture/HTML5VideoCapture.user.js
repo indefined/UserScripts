ca5954603f2a0daba3ac269898da16f0955de87b
indefined <37869994+indefined@users.noreply.github.com>
2025-03-26 12:41:59 +0800

    [H5Capture] 更换CDN为Zstatic

--- HTML5VideoCapture/HTML5VideoCapture.user.js
+++ HTML5VideoCapture/HTML5VideoCapture.user.js
@@ -12,7 +12,7 @@
 // @grant        GM_setValue
 // @grant        GM.getValue
 // @grant        GM.setValue
-// @require      https://cdn.jsdelivr.net/npm/jszip@3.1.5/dist/jszip.min.js
+// @require      https://s4.zstatic.net/ajax/libs/jszip/3.1.5/jszip.min.js
 // @license      MIT
 // ==/UserScript==
 
